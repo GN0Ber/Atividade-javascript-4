41a15230ec0c2897341d618b791a1c1ddaae39d4
Guilherme <gnbernnardo@gmail.com>
2023-04-13 22:36:56 -0300

    Loops e Funcoes

---
+++ script.js
@@ -0,0 +1,112 @@
+/*
+// SWITCH
+
+let teste = parseInt(prompt("Digite uma opção"))
+
+switch(teste){
+    case 1:
+        document.write("Opção 1")
+        break;
+    case 2:
+        document.write("Opção 2")
+        break;
+    case 3:
+        document.write("Opção 3")
+        break;
+    default:
+        document.write("Nenhuma das opções")
+        break;
+}
+
+// INCREMENTO
+
+let valor = 7
+document.write(valor)
+document.write("<br>")
+
+valor = valor + 1
+document.write(valor)
+document.write("<br>")
+
+valor++
+document.write(valor)
+document.write("<br>")
+
+// DECREMENTO
+
+let valor1 = 13
+document.write(valor1)
+document.write("<br>")
+
+valor1 = valor1 - 1
+document.write(valor1)
+document.write("<br>")
+
+valor1--
+document.write(valor1)
+document.write("<br>")
+
+// FOR
+
+for(let i=0; i<10; i++){
+    document.write("O valor é: " +i)
+    document.write("<br>")
+}
+
+// FOR COM ARRAY
+
+let carros= ["carro1","carro2","carro3","carro4"]
+
+for(let i=0; i<carros.length; i++){
+    document.write(carros[i])
+    document.write("<br>")
+}
+
+// FOR COM ARRAY E OF
+
+for(let carro of carros){
+    document.write(carro)
+    document.write("<br>")
+}
+
+// WHILE
+
+let cont= 0
+while(cont < 10){
+    document.write("O valor do while é: " +cont)
+    document.write("<br>")
+    cont++
+}
+
+// DO WHILE
+
+let cont= 0
+
+do{
+    document.write("O valor do while é: " +cont)
+    cont++
+}while (cont == 0)
+
+// FUNÇÃO
+
+function soma(a1, a2){
+    return a1 + a2
+}
+
+document.write(soma(5,4));
+document.write("<br>")
+
+function subtracao(a1, a2){
+    return a1 - a2
+}
+
+document.write(subtracao(5,4));
+document.write("<br>")
+
+function avisar(){
+    alert("Teje Avisado!!")
+}
+
+avisar()
+*/
+
